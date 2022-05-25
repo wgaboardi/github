f493a8b4ccd1995ef43cddf5ece96bd881c06728
Wellington Luiz Gaboardi <wgaboardi@gmail.com>
2022-05-25 19:29:04 -0300

    First commit complete

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Layout from './components/layout';
+import Profile from './components/profile';
+import Repositories from './components/repositories';
+import { ResetCSS } from './global/resetCSS';
+import GithubProvider from './providers/github-provider';
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <GithubProvider>
+          <ResetCSS />
+          <Layout>
+            <Profile />
+            <Repositories />
+          </Layout>
+        </GithubProvider>
+      </main>
+    </>
+  )
+}
+
+
+export default App;
